dd359088c5fb15c93c2d3f5664c58223f6a0d959
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2021-01-27 20:38:22 +0100

    as defined in fluxx

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -3,5 +3,15 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
 }});
 
 var siteConfig = {
-  rootPredicate: { type: 'equals', key: 'taxonKey', value: 5 }
+  rootPredicate: {
+    "type": "in",
+    "key": "datasetKey",
+    "values": [
+      "f455bc89-8b18-4ad2-a982-cb961022a3ab",
+      "0e89ef93-34c2-4942-97f6-9d995edb1f8f",
+      "beefc0e5-520f-446c-8d7a-2e1c35394144",
+      "6297efc0-4442-4e8e-be58-1e0ca7729dac",
+      "dbf97ebc-f931-4b99-8b3b-6fb1375b21bb"
+    ]
+  }
 };
